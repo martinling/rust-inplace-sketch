328ba9a5730542a14e9ad0943f120bf0c61e91c3
Martin Ling <martin-git@earth.li>
2022-10-13 10:47:33 +0100

    Various minor updates and adjustments.

--- sketch.rs
+++ sketch.rs
@@ -19,9 +19,6 @@ fn create_or_fail() -> Result<inplace T, Error>;
 // An 'inplace T' represents a potential value. It can be thought of like a closure that returns
 // a 'T', but with a guarantee that the 'T' value will be created in place where it is needed.
 //
-// It has an internal, compiler-generated initializer which is not externally accessible. It is
-// invoked automatically where required.
-//
 // We also define the type '?inplace T' as a way to accept either a 'T' or 'inplace T', and update
 // existing APIs in the standard library to use this type, e.g:
 
@@ -69,9 +66,8 @@ fn create_foo() -> inplace Foo {
 // later time when the destination is known. The 'create_foo' function returns what is essentially
 // a handle for the potential value.
 //
-// An 'inplace T' must contain everything needed to infallibly construct the 'T' value. In the
-// example above, the effect is similar to if 'create_foo' returned a closure to be evaluated
-// later:
+// An 'inplace T' must contain everything needed to construct the 'T' value. In the example above,
+// the effect is similar to if 'create_foo' returned a closure to be evaluated later:
 
 fn create_foo() -> FnOnce() -> Foo {
     || Foo { a: 123, b: "hello" }
@@ -88,15 +84,16 @@ fn create_foo() -> FnOnce() -> Foo {
 //
 // In this proposal, new syntax allows the usage of in-place initialization to be specified
 // explicitly, such that the intent is always clear. Where the compiler cannot see how to achieve
-// what is specified, errors can be reported reliably and more clearly.
+// what is specified, errors can be reported reliably and clearly.
 //
 // Scope of deferral
 // =================
 //
 // When a compile-time expression is converted from 'T' to 'inplace T', code to construct the 'T'
-// value is deferred to the initializer routine. In the first example above, this conversion was
-// caused by the need to convert a 'Foo' to 'inplace Foo' in order to satisfy the return type of
-// 'create_foo'.
+// value is deferred to the initializer routine. In the simple example above, this conversion was
+// caused by the need to convert a 'Foo' to 'inplace Foo' in order to satisfy the return type, so
+// only the final values of each field were captured and stored with the initializer, and only
+// the actual construction of the complete 'struct Foo' was deferred.
 //
 // By explicitly using 'inplace' with a block, the scope of deferral can be expanded, moving
 // more code to the initializer:
@@ -109,14 +106,13 @@ fn create_thing() -> inplace Thing {
     }
 }
 
-// The scope of deferral is therefore controllable, explicit, and minimised by default. This
-// proposal makes no change to the semantics of existing code.
+// The scope of deferral is therefore controllable, explicit, and minimised by default.
 //
 // Error handling
 // ==============
 //
 // Code that produces an 'inplace T' must complete any error handling which could prevent it from
-// returning a valid 'T' value, prior to the conversion to 'inplace T'. The code deferred to the
+// returning a valid 'T' value, prior to the creation of an 'inplace T'. The code deferred to the
 // initializer must always result in a valid 'T' value:
 
 fn try_create_thing() -> Result<inplace Thing, Error> {
@@ -127,8 +123,16 @@ fn try_create_thing() -> Result<inplace Thing, Error> {
     }
 }
 
-// This constraint does not completely preclude further error handling within the initializer, but
-// it does limit that code to actions that still produce a valid 'T' value.
+// This constraint does not completely disallow further error handling within the initializer, but
+// it does limit that code to outcomes that still produce a valid 'T' value.
+//
+// Code in the initializer must never unwind, since to do so could leave references pointing to an
+// uninitialized value. The compiler must reject the code if it cannot compile the initializer
+// without unwinding.
+//
+// This implies that the compiler must reject any may code in an 'inplace' block that may panic, in
+// any configuration where a panic results in an unwind. However, it would be acceptable to allow
+// panics in the initializer in a configuration where a panic results in an abort.
 //
 // Composition
 // ===========
@@ -155,7 +159,7 @@ fn create_bar() -> Result<inplace Bar, Error> {
 fn create_baz() -> Result<inplace Baz, Error>;
 fn create_quux() -> Result<inplace Quux, Error>;
 
-// We apply the rule that when an 'inplace T' value is used as a field in another 'inplace'
+// We apply a rule that when an 'inplace T' value is used as a field in another 'inplace'
 // structure, its initialization is deferred to the initialization of the outer structure. In other
 // words, inplace expressions can be nested and their initializers will be executed together. Thus,
 // complex structures can be constructed in place by composition of their parts.
@@ -218,63 +222,52 @@ impl<T> DstArray<T> where T: ?Sized {
 // In the case where an 'inplace T' is passed but allocation fails, the initialization is skipped
 // and the 'inplace T' value is dropped unused.
 //
-// Consider the use of this function on a small embedded system:
+// Consider the uses of this function on a small embedded system:
 
-arr.append([1u32; 1_000]);             // Wasteful: created on stack, and copied to new allocation.
+arr.append([1; 1_000]);             // Wasteful: created on stack, and copied to new allocation.
 
-arr.append(inplace [1u32; 1_000]);     // Created in-place: less stack used, copy avoided.
+arr.append(inplace [1; 1_000]);     // Created in-place: less stack used, copy avoided.
 
-arr.append([1u32; 1_000_000]);         // Might run out of stack before even trying to allocate!
+arr.append([1; 1_000_000]);         // Might run out of stack before even trying to allocate!
 
-arr.append(inplace [1u32; 1_000_000]); // Won't deplete stack. Allocation may fail, in which
-                                       // case false is returned and initialization is avoided.
+arr.append(inplace [1; 1_000_000]); // Won't deplete stack. Allocation may fail, in which
+                                    // case false is returned and initialization is avoided.
 
-// Fallible initialization
-// =======================
+// No fallible initialization
+// ==========================
 //
-// Allowing for fallible *allocation* is straightforward in the above example, because it is a
-// generic problem that applies equally to any type that might be inserted into the container.
+// This proposal provides no support for fallible initialization. However, it does support:
 //
-// Allowing for fallible initialization is more awkward, because it would require every method
-// on a container, or other function that accepted an 'inplace T', to be genericised such that
-// it could also return a result indicating failure of the initialization.
+// - Fallible allocation.
+// - Error handling which produces a Result prior to the final initialization.
+// - Composition of error handling by the usual mechanisms, resulting in a compound initializer.
+// - Error handling within the deferred initializer which still produces a valid value.
 //
-// It may be straightforward to adapt this proposal such that rather than just 'inplace T' we
-// have for instance a magic trait 'Inplace<T, E>' in which E is the error type that may be
-// optionally returned by the initializer.
+// We argue that these benefits are sufficient to justify the proposal without support for fallible
+// initialization.
 //
-// However, this would probably require that there be some function call to perform the
-// initialization, which would need to be given a pointer to uninitialized memory and return
-// an Option<E>. As it stands, many uses of the proposed 'inplace' feature can be made entirely
-// without any unsafe code, when used to assign to existing locations such as struct members.
+// In principle, it might be straightforward to adapt this proposal such that rather than just
+// 'inplace T' we have for instance a magic trait 'Inplace<T, E>' in which E is an error type that
+// may be potentially returned by the initializer.
 //
-// The trait in question might look like:
-
-trait Inplace<T, E> {
-    fn layout(&self) -> Layout;
-    fn initialize(self, *mut dest: T) -> Option<E>;
-}
-
-// The problem with this approach is that it reintroduces the problem of API churn. Rather than
-// simply adapting Vec::push() as outlined above, we would need a new call along the lines of:
-
-enum InplaceError<E> {
-    AllocFailure(),
-    InitFailure(E),
-}
-
-impl<T> Vec<T> {
-    pub fn try_push<E>(&mut self, value: ?Inplace<T, E>) -> Result<(), InplaceError>;
-}
-
-// That said, this might still be nicer than the closure-based approach proposed in #2884.
+// However, since there is no way for an assignment to result in an error, the conversion to 'T'
+// would require some other operation to be invoked to perform the initialization and optionally
+// return an error. Assuming this were a function call, it would need to be passed a pointer to
+// uninitialized memory, so would only be usable in an unsafe context. That would be a significant
+// step backwards because as it stands, this proposal allows in place initialization with no unsafe
+// code, except in places where uninitialized memory is already in use, such as within the
+// implementation of a container type.
+//
+// Supporting fallible initialization would further require every method on a container, or other
+// function that accepted an 'inplace T', to be genericised such that it could also return a result
+// indicating failure of the initialization. This would reintroduce the API churn problem.
 //
 // Self-referential structures
 // ===========================
 //
 // With the ability to require that structures be created in place, it becomes possible in
-// combination with the !Unpin trait for self-referential structures to be created safely. This
-// would require changes to the borrow checker as well as some syntactic means of self-reference.
+// combination with pinning for self-referential structures to be created safely. Doing so would
+// require changes to the borrow checker as well as some syntactic means of self-reference.
 //
 // Here, we hypothesise reusing the 'inplace' keyword to refer to the eventual destination of an
 // inplace expression, but other syntax choices could be used:
@@ -295,6 +288,5 @@ impl ListHead {
     }
 }
 
-// This would address the issues affecting the use of Rust in the Linux kernel, as discussed at:
-//
+// This would address issues affecting the use of Rust in the Linux kernel, as discussed at:
 // https://lwn.net/Articles/907876/
